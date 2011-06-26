bf663f39043e221c5958523be76bf199902d7ae3
picard.damien@gmail.com <picard.damien@gmail.com@f60ece42-e4ac-11de-b136-e12340cf38ae>
2011-06-26 06:59:00 +0000

    Remove Ant project

--- trunk/src/com/axeiya/gwtckeditor/client/events/SaveHandler.java
+++
@@ -1,7 +0,0 @@
-package com.axeiya.gwtckeditor.client.events;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface SaveHandler<T> extends EventHandler {
-	void onSave(SaveEvent<T> event);
-}
